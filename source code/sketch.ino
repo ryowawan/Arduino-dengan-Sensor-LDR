c10319e1f0beabffe7eb4c0c5d8c2530e3af5b40
ryo_wawan <senjuwawanrio@gmail.com>
2026-06-11 21:34:50 +0700

    Create sketch.ino

---
+++ source code/sketch.ino
@@ -0,0 +1,28 @@
+const int ledPin = 13;
+const int ldrPin = A0;
+
+void setup() {
+Serial.begin(9600);
+  
+pinMode(ledPin, OUTPUT);
+pinMode(ldrPin, INPUT);
+}
+
+void loop() {
+
+int ldrStatus = analogRead(ldrPin);
+
+if (ldrStatus > 100) {
+digitalWrite(ledPin, HIGH);
+Serial.print("Its DARK, Turn on the LED : ");
+Serial.println(ldrStatus);
+
+} else {
+
+digitalWrite(ledPin, LOW);
+Serial.print("Its BRIGHT, Turn off the LED : ");
+Serial.println(ldrStatus);
+
+}
+
+}
